fe78523896b419c326f69c203416aae01411a9f1
paul_342@sina.com <paul_342@sina.com>
2016-06-16 11:25:58 +0800

    ElasticSearchDemo

---
+++ ElasticSearchOriginalTest/src/com/guozz/elasticsearch/test1/dennisit/util/JsonUtil.java
@@ -0,0 +1,35 @@
+package com.guozz.elasticsearch.test1.dennisit.util;
+
+import java.io.IOException;
+
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import com.guozz.elasticsearch.test1.dennisit.data.Medicine;
+
+
+public class JsonUtil {
+
+	/**
+     * 实现将实体对象转换成json对象
+     * @param medicine    Medicine对象
+     * @return
+     */
+    public static String obj2JsonData(Medicine medicine){
+        String jsonData = null;
+        try {
+            //使用XContentBuilder创建json数据
+            XContentBuilder jsonBuild = XContentFactory.jsonBuilder();
+            jsonBuild.startObject()
+            .field("id",medicine.getId())
+            .field("name", medicine.getName())
+            .field("funciton",medicine.getFunction())
+            .endObject();
+            jsonData = jsonBuild.string();
+            System.out.println(jsonData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+}
