0cfb7f9a7a5a1306a274f60a8be7215122625848
guozhizhong <guozhizhong@D8BYXB2.letv.local>
2016-11-24 13:46:57 +0800

    小修改

--- practiseEveryDay/src/com/guozz/test/hidePhoneNum/Test.java
+++ practiseEveryDay/src/com/guozz/test/hidePhoneNum/Test.java
@@ -22,21 +22,29 @@ public class Test {
 	}
 	
 	/**
-	 * 
+	 * <p>
+	 * <blockquote><pre>
+	 *   例：phoneNum :15313719308
+	 *   	regExp:(\\d{3})\\d{4}(\\d{4})
+	 *   	hideExp:$1****$2
+	 *   	return    153****9308
+	 * </pre></blockquote>
+	 * </p>
 	 * @param str
 	 * @param regExp
 	 * @param hideExp
 	 * @return
 	 * @throws IllegalAccessException
+
 	 */
-	public static String hideMiddleByRegexp(String str,String regExp,String hideExp) throws IllegalAccessException{
-		if(str==null || "".equals(str)){
+	public static String hideMiddleByRegexp(String phoneNum,String regExp,String hideExp) throws IllegalAccessException{
+		if(phoneNum==null || "".equals(phoneNum)){
 			return "";
 		}
 		if(regExp==null || hideExp==null){
 			throw new IllegalAccessException("传入参数错误,传入的正则表达式和隐藏表达式不能为null");
 		}
-		return str.replaceAll(regExp,hideExp);
+		return phoneNum.replaceAll(regExp,hideExp);
 	}
 
 }
