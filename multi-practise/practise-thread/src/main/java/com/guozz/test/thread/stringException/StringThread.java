301d6acda4ee2fda38f0e117246dffa7071063e7
Administrator <Administrator@6CR63604J3>
2017-04-27 11:18:38 +0800

    测试

---
+++ multi-practise/practise-thread/src/main/java/com/guozz/test/thread/stringException/StringThread.java
@@ -0,0 +1,38 @@
+package com.guozz.test.thread.stringException;
+
+public class StringThread implements Runnable {
+
+	private static final String LOCK_PREFIX = "XXX---";
+	
+	private String ip;
+	
+	
+	public StringThread(String ip){
+		this.ip=ip;
+		
+	}
+	
+	public void run() {
+		  String lock = buildLock();
+		  	synchronized (lock) {
+	            System.out.println("[" + Thread.currentThread().getName() + "]开始运行了");
+	            // 休眠5秒模拟脚本调用
+	            try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	            System.out.println("[" + Thread.currentThread().getName() + "]结束运行了");
+	        }				
+	}
+	
+	private String buildLock() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LOCK_PREFIX);
+        sb.append(ip);
+        String lock = sb.toString().intern();
+        System.out.println("[" + Thread.currentThread().getName() + "]构建了锁[" + lock + "]");
+        return lock;
+    }
+
+}
