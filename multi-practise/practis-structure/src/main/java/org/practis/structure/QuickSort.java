dae47ea1f13da0bb181af187d4c38fab0bcc65b0
郭智忠 <@192.168.158.105>
2017-07-24 09:45:14 +0800

    优化格式

--- multi-practise/practis-structure/src/main/java/org/practis/structure/QuickSort.java
+++ multi-practise/practis-structure/src/main/java/org/practis/structure/QuickSort.java
@@ -13,18 +13,15 @@ public class QuickSort {
     public static int getMiddle(int[] numbers, int low,int high)
     {
         int temp = numbers[low]; //数组的第一个作为中轴
-        while(low < high)
-        {
-        while(low < high && numbers[high] > temp)
-        {
-            high--;
-        }
-        numbers[low] = numbers[high];//比中轴小的记录移到低端
-        while(low < high && numbers[low] < temp)
-        {
-            low++;
-        }
-        numbers[high] = numbers[low] ; //比中轴大的记录移到高端
+        while(low < high){
+	        while(low < high && numbers[high] > temp){
+	            high--;
+	        }
+	        numbers[low] = numbers[high];//比中轴小的记录移到低端
+	        while(low < high && numbers[low] < temp){
+	            low++;
+	        }
+	        numbers[high] = numbers[low] ; //比中轴大的记录移到高端
         }
         numbers[low] = temp ; //中轴记录到尾
         return low ; // 返回中轴的位置
