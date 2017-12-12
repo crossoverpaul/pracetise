2213e75fee66aa811c3cf71ab7420b8bd4d2a801
guozhizhong <guozhizhong@czb365.com>
2017-12-12 11:55:14 +0800

    提交

---
+++ lua-practise/src/test-String.lua
@@ -0,0 +1,12 @@
+String1 ="Lua"
+print("\"字符串 1 是\"",String1);
+
+string2 = 'runoob.com'
+print("字符串 2 是",string2)
+
+string3 = [["Lua 教程"]]
+print("字符串 3 是",string3)
+
+ print(string.gsub("aaaa","a","z",3));
+ 
+ print(string.format("the value is:%d",4));
