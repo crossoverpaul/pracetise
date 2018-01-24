897f77ff8b0f6beda47b07bf67fc136e30e16221
guozhizhong <guozhizhong@czb365.com>
2018-01-24 10:09:02 +0800

    修改

--- multi-practise/practise-upgrade/src/main/java/org/practise/upgrade/pattern/template/Client.java
+++ multi-practise/practise-upgrade/src/main/java/org/practise/upgrade/pattern/template/Client.java
@@ -3,7 +3,7 @@ package org.practise.upgrade.pattern.template;
 public class Client {
 
 	public static void main(String[] args) {
-		TakeMoney takeMoney = new TakeMoney();
+		BankTemplateMethod takeMoney = new TakeMoney();
 		takeMoney.process();
 	}
 }
