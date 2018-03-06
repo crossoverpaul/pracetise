8caf2877d05653e141f7df60c669f576e9bdf6a1
guozhizhong <guozhizhong@czb365.com>
2018-03-06 10:37:25 +0800

    提交

---
+++ multi-practise/practise-storm/src/main/java/org/practise/storm/App.java
@@ -0,0 +1,13 @@
+package org.practise.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
