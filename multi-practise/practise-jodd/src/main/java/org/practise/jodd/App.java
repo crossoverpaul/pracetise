dbc77d69d5b10e2a30c44b5b9d1309ee4ad61a00
guozhizhong <guozhizhong@czb365.com>
2018-03-28 16:24:20 +0800

    提交

--- multi-practise/practise-jodd/src/main/java/org/practise/jodd/App.java
+++ multi-practise/practise-jodd/src/main/java/org/practise/jodd/App.java
@@ -1,5 +1,10 @@
 package org.practise.jodd;
 
+import java.util.HashMap;
+
+import jodd.bean.BeanUtilBean;
+import jodd.util.PrettyStringBuilder;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println(PrettyStringBuilder.str(
+    			
+    	new HashMap<String,String>() {
+    		{
+    			put("a", "1");
+    			put("b", "2");
+    			put("c", "3");
+    		}
+    	}
+    	
+    			));
     }
 }
