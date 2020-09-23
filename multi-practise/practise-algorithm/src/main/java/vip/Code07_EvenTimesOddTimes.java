95921a38995615f2248137df2b50188c0753d3d1
guozhizhong <guo_zhizhong@sina.com>
2020-09-23 10:31:07 +0800

    XOR

---
+++ multi-practise/practise-algorithm/src/main/java/vip/Code07_EvenTimesOddTimes.java
@@ -0,0 +1,21 @@
+package vip;
+
+public class Code07_EvenTimesOddTimes {
+
+    // arr中，只有一种数，出现奇数次
+    public static void printOddTimesNum(int [] arr){
+        int eor=0;
+        if(arr.length==0){
+            System.out.println(0);
+        }
+        for(int i=0;i<arr.length;i++){
+            eor=eor^arr[i];
+        }
+        System.out.println(eor);
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,1,3,3,3,3,3,3,4};
+        printOddTimesNum(arr);
+    }
+}
