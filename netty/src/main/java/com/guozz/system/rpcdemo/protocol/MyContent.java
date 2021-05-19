4eed1a668701eea090a888835ab6f4066ea3d43a
guozhizhong <guo_zhizhong@sina.com>
2021-05-19 14:23:35 +0800

    新增

--- netty/src/main/java/com/guozz/system/rpcdemo/protocol/MyContent.java
+++ netty/src/main/java/com/guozz/system/rpcdemo/protocol/MyContent.java
@@ -1,5 +1,7 @@
 package com.guozz.system.rpcdemo.protocol;
 
+import java.io.Serializable;
+
 /**
  * @ClassName MyContent
  * @Description TODO
@@ -27,6 +29,52 @@ package com.guozz.system.rpcdemo.protocol;
  * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  * -----------------------------------------------------佛祖保佑--------永无BUG
  */
-public class MyContent {
+public class MyContent implements Serializable {
+
+    String name;
+    String methodName;
+    Class<?>[] parameterTypes;
+    Object[] args;
+    Object res;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+
+    public Object getRes() {
+        return res;
+    }
+
+    public void setRes(Object res) {
+        this.res = res;
+    }
 }
 
